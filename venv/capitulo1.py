b20710564f26a043904597da0467c29d1f616a70
Victor M. Blanco Mancera <blanko_1490@hotmail.com>
2020-12-26 09:43:19 -0500

    Actualizacion de contenido e importaciones de paquete

--- venv/capitulo1.py
+++ venv/capitulo1.py
@@ -1,26 +1,26 @@
 ######################## LEER IMAGEN ############################
 
-# importacion del paquete
+ #importacion del paquete
 
-# import cv2
+ import cv2
 
 #prueba para corroborar la importacion adecuada del los paquetes importados
 
-# print ("packege  imported")
+ print ("packege  imported")
 
 #cargamos una imagen usando imread
 # esta imagen esta almacenda en el directorio de nuestro proyecto en la carpeta recursos
 
-# img=cv2.imread("../recursos/lena.png")
+ img=cv2.imread("../recursos/lena.png")
 
 #usamos el comando imshow para mostrala
 
-#cv2.imshow("Output",img)
+cv2.imshow("Output",img)
 
 # waitKey(0)mostrará la ventana infinitamente hasta que se presione cualquier tecla
 # (es adecuada para la visualización de imágenes).
 
-# cv2.waitKey(0)
+ cv2.waitKey(0)
 
 
 ######################### READ VIDEO #############################
@@ -56,15 +56,15 @@
 
 
 ######################### READ WEBCAM  ############################
-import cv2
-frameWidth = 640
-frameHeight = 480
-cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10,150)
-while True:
-    success, img = cap.read()
-    cv2.imshow("Result", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#import cv2
+#frameWidth = 640
+#frameHeight = 480
+#cap = cv2.VideoCapture(0)
+#cap.set(3, frameWidth)
+#cap.set(4, frameHeight)
+#cap.set(10,150)
+#while True:
+#    success, img = cap.read()
+#    cv2.imshow("Result", img)
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#        break
