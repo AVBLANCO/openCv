c8a8385e3ca96780bf82c82b7d28db87f9fc2fb6
Victor M. Blanco Mancera <blanko_1490@hotmail.com>
2020-12-26 12:16:23 -0500

    Actualizacion de contenido e importaciones de paquete final

--- venv/capitulo1.py
+++ venv/capitulo1.py
@@ -1,70 +1,70 @@
 ######################## LEER IMAGEN ############################
-
- #importacion del paquete
-
- import cv2
-
+"""
+# importacion del paquete
+import cv2
 #prueba para corroborar la importacion adecuada del los paquetes importados
-
- print ("packege  imported")
-
+print ("packege  imported")
 #cargamos una imagen usando imread
 # esta imagen esta almacenda en el directorio de nuestro proyecto en la carpeta recursos
 
- img=cv2.imread("../recursos/lena.png")
-
+img=cv2.imread("../recursos/lena.png")
 #usamos el comando imshow para mostrala
 
 cv2.imshow("Output",img)
-
 # waitKey(0)mostrará la ventana infinitamente hasta que se presione cualquier tecla
 # (es adecuada para la visualización de imágenes).
 
- cv2.waitKey(0)
-
+cv2.waitKey(0)
+"""
 
 ######################### READ VIDEO #############################
+"""
+import cv2
 
-#import cv2
+cap = cv2.VideoCapture("recursos/test_video.mp4")
+print(cap)
+while True:
+    success, img =cap.read()
+    cv2.imshow("Video Salida",img)
+    if cv2.waitKey(1)& 0xFF ==ord('q'):
+        break
 
-#cap = cv2.VideoCapture("recursos/test_video.mp4")
-#print(cap)
-#while True:
-#    success, img =cap.read()
-#    cv2.imshow("Video Salida",img)
-#    if cv2.waitKey(1)& 0xFF ==ord('q'):
-#        break
-
-
+"""
 ######################### LEER VIDEO  2  #############################
+"""
+import cv2
+frameWidth = 640
+frameHeight = 480
+dim=(frameWidth,frameHeight)
+cap = cv2.VideoCapture("recursos/test_video.mp4")
+print(cap)
+while True:
+    success, img =cap.read()
+    img = cv2.resize(img, (frameWidth, frameHeight))
+    img = cv2.resize(img,dim)
+    cv2.rectangle(img, (100, 100), (200, 200), [255, 0, 0], 2)
+    cv2.imshow("Video Salida",img)
+    if cv2.waitKey(1)& 0xFF ==ord('q'):
+        break
 
-#import cv2
-#frameWidth = 640
-#frameHeight = 480
-#dim=(frameWidth,frameHeight)
-#cap = cv2.VideoCapture("recursos/test_video.mp4")
-#print(cap)
-#while True:
-  #  success, img =cap.read()
-#    img = cv2.resize(img, (frameWidth, frameHeight))
- #   img = cv2.resize(img,dim)
-#    cv2.rectangle(img, (100, 100), (200, 200), [255, 0, 0], 2)
- #   cv2.imshow("Video Salida",img)
- #   if cv2.waitKey(1)& 0xFF ==ord('q'):
- #       break
-
-
+"""
 
 ######################### READ WEBCAM  ############################
-#import cv2
-#frameWidth = 640
-#frameHeight = 480
-#cap = cv2.VideoCapture(0)
-#cap.set(3, frameWidth)
-#cap.set(4, frameHeight)
-#cap.set(10,150)
-#while True:
-#    success, img = cap.read()
-#    cv2.imshow("Result", img)
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        break
+
+import cv2
+frameWidth = 640
+frameHeight = 480
+#videocapture
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10,150)
+while True:
+    success, img = cap.read()
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+
--- venv/otroEjemplo.py
+++ venv/otroEjemplo.py
@@ -1,26 +1,38 @@
 import cv2
+#reconocimiento de los caracteres de la placa
 import pytesseract
-
+#ruta de tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+#almacenar la placa
 placa = []
+#cagar la imagen a analizar
 image = cv2.imread('../recursos/auto001.jpg')
+#image = cv2.imread('../recursos/auto2.png')
+#image = cv2.imread('../recursos/descarga8.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#atenuar el ruido
 gray = cv2.blur(gray, (3, 3))
+#engrozar areas blancas
 canny = cv2.Canny(gray, 150, 200)
 canny = cv2.dilate(canny, None, iterations=1)
+#version vieja de cv2
 # _,cnts,_ = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+# trabajar con los contornos
 cnts, _ = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 # cv2.drawContours(image,cnts,-1,(0,255,0),2)
 for c in cnts:
     area = cv2.contourArea(c)
+    #identificar un rectangulo
     x, y, w, h = cv2.boundingRect(c)
     epsilon = 0.09 * cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, epsilon, True)
-
+#condicion de que tenga 4 vertices
     if len(approx) == 4 and area > 9000:
         print('area=', area)
         # cv2.drawContours(image,[approx],0,(0,255,0),3)
         aspect_ratio = float(w) / h
+        #2.4
         if aspect_ratio > 2.4:
             placa = gray[y:y + h, x:x + w]
             text = pytesseract.image_to_string(placa, config='--psm 11')
@@ -32,4 +44,5 @@ for c in cnts:
 
 cv2.imshow('Image', image)
 cv2.moveWindow('Image', 45, 10)
+#el proceso siga activo hasta precionar una tecla
 cv2.waitKey(0)
